5a6bbe15403e70ad94669773e46d3b3346a49b68
Adevalter <adevalter@hotmail.com>
2023-05-29 15:15:26 -0300

    Iniciado class adapter

--- app/src/main/java/br/com/adeweb/testecursokotlinapiimgur/adapter/ImgurAdapeter.kt
+++ app/src/main/java/br/com/adeweb/testecursokotlinapiimgur/adapter/ImgurAdapeter.kt
@@ -1,4 +1,24 @@
 package br.com.adeweb.testecursokotlinapiimgur.adapter
 
-class ImgurAdapeter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.com.adeweb.testecursokotlinapiimgur.databinding.ItemImgurBinding
+
+class ImgurAdapeter() : RecyclerView.Adapter<ImgurAdapeter.ImgurViewHolder>() {
+    inner class ImgurViewHolder(ItemImgur : ItemImgurBinding) : RecyclerView.ViewHolder(ItemImgur.root) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImgurViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ImgurViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
 }
