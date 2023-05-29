fe23cce197fa73acff449681124650ace54d46cb
Adevalter <adevalter@hotmail.com>
2023-05-29 15:09:27 -0300

    configurado binding

--- app/src/main/java/br/com/adeweb/testecursokotlinapiimgur/MainActivity.kt
+++ app/src/main/java/br/com/adeweb/testecursokotlinapiimgur/MainActivity.kt
@@ -2,10 +2,16 @@ package br.com.adeweb.testecursokotlinapiimgur
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.adeweb.testecursokotlinapiimgur.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy{
+        ActivityMainBinding.inflate( layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView( binding.root )
     }
 }
